3964017bd8d161ae663714fbe668bda36a3d0095
robutseverywhere <43159681+robutseverywhere@users.noreply.github.com>
2019-01-06 20:53:11 -0500

    Add files via upload

---
+++ threadedspaceflower.py
@@ -0,0 +1,24 @@
+# Python3.7.1
+import turtle
+
+turtle.hideturtle()
+turtle.speed(speed=10)
+
+
+for i in range(100):
+    turtle.color('red')
+    turtle.forward(60)
+    turtle.left(90)
+    turtle.backward(40)
+    turtle.right(50)
+    turtle.forward(60)
+    turtle.left(60)
+    turtle.backward(60)
+    turtle.color('black')
+    turtle.circle(25*i)
+    turtle.circle(-25*i)
+    turtle.left(i)
+
+turtle.end_fill()
+
+turtle.done()
